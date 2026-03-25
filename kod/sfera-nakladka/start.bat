b1856f1ebd72efdac7ea5affc711365f373be9cf
kdywan_254049 <idywan_254049@e-science.pl>
2026-03-25 22:52:57 +0100

    Zmiana portów aplikacji nakładki

--- kod/sfera-nakladka/start.bat
+++ kod/sfera-nakladka/start.bat
@@ -2,7 +2,7 @@
 
 cd /d "%~dp0"
 
-start "" /b php\php.exe -c php\php.ini-production -S 127.0.0.1:80 -t htdocs
+start "" /b php\php.exe -c php\php.ini-production -S 127.0.0.1:81 -t htdocs
 set PHP_PID=%!
 
 start "" /b javaw -jar aplikacja-allegro_subiekt-integracja.jar
